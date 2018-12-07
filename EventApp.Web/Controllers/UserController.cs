c1fe444dec4ca902ed7a3ce2e5a9a72110eca59b
astonished12 <dan_cehan@yahoo.com>
2018-12-07 10:48:56 +0000

    add web project

---
+++ EventApp.Web/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventApp.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventApp.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public UserController(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Current(int id)
+        {
+            if (id != 1)
+            {
+                return StatusCode(404);
+            }
+
+            UserModel userModel = new UserModel() { Id = 1, Address = "Addres", Age = 12, Email = "d@d.d", FirstName = "Gelu", Height = 1.5f, Weight = 24, LastName = "Popescu", Phone = "0744521321" };
+            return View(userModel);
+        }
+
+        [Route("admin/user/getusers", Name = "admin")]
+        public IActionResult GetUsers()
+        {
+            IEnumerable<UserModel> users = new List<UserModel>() {
+             new UserModel() { Id = 1, Address = "Addres", Age = 12, Email = "d@d.d", FirstName = "Gelu", Height = 1.5f, Weight = 24, LastName = "Popescu", Phone = "0744521321" },
+             new UserModel() { Id = 2, Address = "Addres", Age = 12, Email = "d@d.d", FirstName = "Gelu1", Height = 1.5f, Weight = 24, LastName = "Popescu", Phone = "0744521321" },
+             new UserModel() { Id = 3, Address = "Addres", Age = 12, Email = "d@d.d", FirstName = "Gelu2", Height = 1.5f, Weight = 24, LastName = "Popescu", Phone = "0744521321" },
+             new UserModel() { Id = 4, Address = "Addres", Age = 12, Email = "d@d.d", FirstName = "Gelu3", Height = 1.5f, Weight = 24, LastName = "Popescu", Phone = "0744521321" }  };
+
+            return View(users);
+        }
+
+        public IActionResult GetMyIp()
+        {
+            IpModel ipModel = new IpModel() { IpAddress = httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString() };
+            return View(ipModel);
+        }
+    }
+}
---
+++ EventApp.Web/Models/IpModel.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventApp.Web.Models
+{
+    public class IpModel
+    {
+        public string IpAddress { get; set; }
+    }
+}
